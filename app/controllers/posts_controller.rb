62ce0b9e912a4d0d4a576bdea295897a3e202ade
semen <semensid2020@gmail.com>
2023-05-26 17:51:15 +0700

    2 LESSON. D. Шаблон создания постов. Экшны NEW и CREATE
    - Создали шаблон views/posts/new.html.erb
    При этом: сделали так, чтобы в дропдауне при выборе категории, к которой будет относиться пост, названия доступных категорий подтянулись (из БД)  ->  в поле  name  динамически с помощью шаблонизатора.
    - Чтобы это работало, мы в контроллере постов создаем метод def new,  а в нём переменную @topics, которую с бэка и отдаем.
    
    Далее создадим непосредственно обработчик (handler) для отправляемого с этой страницы запроса на создание поста.
    Обрабатывать этот запрос будет конечно же метод DEF CREATE в контроллере постов.
    
    - Создали метод def create в контроллере постов, в нём же внесли разрешенные параметры:   permit(:title, :body, :topic_id)
    - В routes.rb внесли роут для экшенов NEW и  CREATE. 									 _15:49_

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,17 @@ class PostsController < ApplicationController
 
   def show
   end
+
+  def new
+    @topics = Topic.all()
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :body, :topic_id))
+    @post.save
+
+    # Редиректим на страницу показа поста.
+    # Что касается адреса: тут идет конкатенация двух частей в один URL, а-ля  /posts/15
+    redirect_to('/posts/' + @post.id.to_s)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'posts/index'
+  get 'posts/new', to: "posts#new"
   get 'posts/:id', to: "posts#show"
+  post 'posts/create', to: "posts#create"
 
   root to: "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
