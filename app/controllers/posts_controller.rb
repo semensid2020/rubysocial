62def579d6dab998536b8ca1d8c4bf757acf820a
semen <semensid2020@gmail.com>
2023-05-26 23:11:15 +0700

    2 LESSON. G. Реализована возможность редактирования постов.
    Теперь необходимо добавить возможность редактирования постов -   шаблон  posts/edit.html.erb
    
    Этот будет такой некий гибрид шаблонов SHOW  и  NEW.
    От NEW мы возьмём формочку, а от SHOW мы возьмем тэги шаблонизатора (?)
    
    Т.е. нам нужно показать НЕ пустую форму, как на странице создания поста, а нам нужно подтянуть данные уже существующие по этому посту и вставить их в соответствующие поля на странице (title, body…)
    
    Ну и поменять handler формы  (экшн,  читай ПУТЬ )
    
    Под тэгами шаблонизатора она видимо имеет в виду то, что мы добавляем:
    1.Атрибут value="<%= @post.title %>"
    
    2. Что касается топика, то нам нужно показать опцию как выбранную, если  id поста совпадает с  текущей опцией.
    Т.е. нам нужно её автоматически показать как выбранную.
    
    Это мы тоже делаем динамически:
    чтобы выбрать конкретную опцию нужно использовать атрибут selected тэга option:
    т.е.    <% if @post.topic_id == topic.id %>selected=selected
    
    3. в textarea мы вставляем текущий текст поста из базы:    <%= @post.body %>
    
    4. в хэндлер update (читай В УРЛ) мы добавим id поста:  <form action="/posts/update/<%= @post.id %>"
    
    =  =  =  =  =  =  =  =  =  =  =  =  =  =
    
    Далее нам нужно создать хэндлер для экшнов EDIT и  UPDATE  в контроллере постов. Добавили.
    Далее добавили в роутах пути к ним.
    
    Также легкий рефакторинг:
    params.require(:post).permit(:title, :body, :topic_id)   вынесено в отдельный метод  post_params 		 _25:05_

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,16 +7,33 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    # Топики забираем, чтобы отрендирить в select'е доступные для выбора категории постов (топики)
+    @topics = Topic.all()
+    @post = Post.find(params[:id])
+  end
+
   def new
     @topics = Topic.all()
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:title, :body, :topic_id))
+    @post = Post.new(post_params)
     @post.save
 
     # Редиректим на страницу показа поста.
     # Что касается адреса: тут идет конкатенация двух частей в один URL, а-ля  /posts/15
     redirect_to('/posts/' + @post.id.to_s)
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to '/posts/' + @post.id.to_s
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :topic_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'posts/new', to: "posts#new"
   get 'posts/:id', to: "posts#show"
   post 'posts/create', to: "posts#create"
+  get 'posts/edit/:id', to: "posts#edit"
+  post 'posts/update/:id', to: "posts#update"
 
   root to: "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
